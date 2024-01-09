d30bb102345ff3d8e029f0609ef30cd96c9aa8ea
Yevhen02 <153821695+Yevhen02@users.noreply.github.com>
2024-01-09 13:30:37 +0200

    updated task-2 private property

--- js/task-2.js
+++ js/task-2.js
@@ -1,26 +1,44 @@
 class Storage {
+  #items;
+
   constructor(items) {
-    this.items = items;
+    this.#items = items;
   }
+
   getItems() {
-    return this.items;
+    return this.#items;
   }
+
   addItem(newItem) {
-    this.items.push(newItem);
+    this.#items.push(newItem);
   }
+
   removeItem(itemToRemove) {
-    const index = this.items.indexOf(itemToRemove);
+    const index = this.#items.indexOf(itemToRemove);
     if (index !== -1) {
-      this.items.splice(index, 1);
+      this.#items.splice(index, 1);
     }
   }
 }
 
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+// виклик функції 
+console.log(storage.getItems()); 
+// ["Nanitoids", "Prolonger", "Antigravitator"]
+
+// додаємо item і змінюємо масив 
 storage.addItem("Droid");
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+// виклик функції 
+console.log(storage.getItems()); 
+// ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+// прибираємо item і змінюємо масив 
 storage.removeItem("Prolonger");
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+// виклик функції 
+console.log(storage.getItems()); 
+// ["Nanitoids", "Antigravitator", "Droid"]
 
 console.log("..................");
